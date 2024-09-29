9d5e6d9831049d46ac7a1766dccfbecbd2265c6c
Anderson Gomes <54241354+Nosredzzz@users.noreply.github.com>
2024-09-29 09:27:53 -0300

    Create git issues

--- lambdas/channels_config.py
+++ lambdas/channels_config.py
@@ -7,6 +7,7 @@ from telegram_handler import TelegramHandler
 # Mapping of channel names to their respective handler classes
 CHANNELS_HANDLER_CLASS_MAP = {
     "telegram": TelegramHandler,
-    "whatsapp": "WhatsAppHandler",
-    "messenger": "MessengerHandler",
+    "whatsapp": "WhatsAppHandler", # TODO: #9 Implement the WhatsAppHandler class
+    "messenger": "MessengerHandler", #TODO: #10 Implement the MessengerHandler class
+    # Add more handlers here as needed
 }
